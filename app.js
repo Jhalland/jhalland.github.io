d5baa2d7614bd85c462f97e9a510428508606e4f
Jhalland <84104528+Jhalland@users.noreply.github.com>
2021-06-10 18:29:05 -0600

    Websocket On Open Change

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var diceNmod;
 const ws = new WebSocket("wss://dicewebsocket.glitch.me/:3000");
 var threeHourCount = 0;
 
-setTimeout(beginTimeOutCount(), 5000);
+ws.onopen(setTimeout(beginTimeOutCount(), 50000));
+
 
 ws.addEventListener("open", function () {
     document.getElementById("roll-button").addEventListener("click", rollDice);
